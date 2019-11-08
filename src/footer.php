c7cc9a341212ec14a6a2ed73edbb1989d52c803b
KindPixel <thomas.lima@viacesi.fr>
2019-11-08 13:39:54 +0100

    footer

--- src/footer.php
+++ src/footer.php
@@ -3,11 +3,9 @@
     include("head.php");
     ?>
     <div class="footer">
-        <div class="footer-icon">
-            <a href="https://www.instagram.com/"> <i class="fab fa-instagram"></i></a>
-            <a href="https://twitter.com/" > <i class="fab fa-twitter"></i></a>
-            <a href="https://facebook.com/" > <i class="fab fa-facebook"></i></a>
-        </div>
-            <a style="text-center" href="/routes/mentions_legales.html">Mentions légales</a>
+            <a class="footer-icon" href="https://www.instagram.com/"> <i class="fab fa-instagram"></i></a>
+            <a class="footer-icon" href="https://twitter.com/" > <i class="fab fa-twitter"></i></a>
+            <a class="footer-icon" href="https://facebook.com/" > <i class="fab fa-facebook"></i></a>
+            <a style="margin-left:20px; color:whitesmoke" href="/routes/mentions_legales.html">Mentions légales</a>
     </div>
 </footer>
