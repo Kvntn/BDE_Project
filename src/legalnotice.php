ea503fe2c1282837fccf50d029f27f43a7cbe10d
GrineFather <44920911+GrineFather@users.noreply.github.com>
2019-11-12 10:24:16 +0100

    create and update of legalnotice

--- src/legalnotice.php
+++ src/legalnotice.php
@@ -6,6 +6,7 @@
     include("footer.php");
     include("aside.php");
 ?>
+
 <div class="legal-container">
 <h1>Éditeur : Association CESI</h1>
 <p>SIREN : 775 722 572
@@ -34,12 +35,12 @@ e-mail :
 <a href="mailto:contact@poissonbouge.fr">contact@poissonbouge.fr</a></p>
 <h1>Respect de la vie privée et collecte des Données Personnelles<strong><br> </strong></h1>
 <p>Soucieux de protéger la vie privée de ses clients, CESI s’engage dans la protection des données personnelles. Une politique sur la protection des données personnelles rappelle nos principes et nos actions visant au respect de la réglementation applicable en matière de protection des données à caractère personnel.</p>
-<p><a href="https://www.cesi.fr/politique-de-confidentialite/">Pour lire l’intégralité de notre politique sur la Protection des données personnelles cliquez-ici</a>
+<p><a href="privacy_policy.php">Pour lire l’intégralité de notre politique sur la Protection des données personnelles cliquez-ici</a>
 </p><h1>Sécurité</h1>
 <p>Le CESI s’engage à mettre en œuvre tous les moyens nécessaires au bon fonctionnement du site. Cependant, le CESI ne peut pas garantir la continuité absolue de l’accès aux services proposés par le site. Les adhérents sont informés que les informations et services proposés sur le site pourront être interrompus en cas de force majeure et pourront le cas échéant contenir des erreurs techniques.</p>
 <h1>Utilisation de cookies<strong><br> </strong></h1>
 <p>Des cookies sont utilisés sur nos sites.</p>
-<p><a href="https://www.cesi.fr/politique-de-confidentialite/">Pour plus d’informations, vous pouvez vous référer à la Politique sur le Protection des Données Personnelles en cliquant-ici</a></p>
+<p><a href="privacy_policy.php">Pour plus d’informations, vous pouvez vous référer à la Politique sur le Protection des Données Personnelles en cliquant-ici</a></p>
 <h1>Déclarations d’activité</h1>
 <p>CESI SAS – Société par actions simplifiée au capital de 1.1M€
 <br> 342 707 502
@@ -61,5 +62,6 @@ Déclaration d’activité enregistrée sous le numéro 11 75 47883 75 auprès d
 <br> 
 Cet enregistrement ne vaut pas agrément de l’État.</p>
 </div>
+<br>
 </html>
 
