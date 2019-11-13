e7fcdf889ebbba4b4bb5c179b54d52b37ba104f0
KindPixel <thomas.lima@viacesi.fr>
2019-11-13 12:18:46 +0100

    shop + product

--- src/nav.php
+++ src/nav.php
@@ -1,6 +1,6 @@
 
 <nav class="mb-1 navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="index.php">B.D.E.</a>
+  <a class="navbar-brand" href="index.php">BDE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
     aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
