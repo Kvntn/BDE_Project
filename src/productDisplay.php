803ca876d20e01cd11e13dac3db800d7c8075ac5
sfr <saija.fahdrachid@viacesi.fr>
2019-11-13 15:39:08 +0100

    shop fix

--- src/productDisplay.php
+++ src/productDisplay.php
@@ -14,14 +14,14 @@ class Product
 		echo '
 		<div class="col-md-4">
 		<div class="card mb-4 box-shadow">
-            <img class="card-img-top" style="max-width= 250px" src="',$key['Photo'],'">
+            <img class="card-img-top" style="max-width: 350px; height:400px;" src="',$key['Photo'],'">
             <div class="card-body">
-				<p class="card-text">',$key['Description'],'</p>
+				<p class="card-text">',$key['NomProduit'],'</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">Acheter</button>
                     </div>
-                    <small class="text-muted">',$key['Prix'],'</small>
+                    <small class="text-muted">',$key['Prix'],'€</small>
 				</div>
 			</div>
 		</div>
