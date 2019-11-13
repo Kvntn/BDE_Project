9827a5b519de8f7608a3ee15cdcb65d7af97af6c
sfr <saija.fahdrachid@viacesi.fr>
2019-11-13 11:49:03 +0100

    beta version of shop

--- src/shop.php
+++ src/shop.php
@@ -1,107 +1,53 @@
 <!doctype html>
 <html lang="fr">
-
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 <body>
-        <div class="album py-5 bg-light">
-          <div class="container-shop">
-      
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_a3.jfif">
-                  <div class="card-body">
-                    <p class="card-text">Audi A3</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                          View
-                        </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_a3.jfif">                  <div class="card-body">
-                    <p class="card-text">Audi A3</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                              View
-                            </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_a3.jfif">                  <div class="card-body">
-                    <p class="card-text">Audi A3</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                              View
-                            </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-      
-      
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_r8.jpg">                   <div class="card-body">
-                    <p class="card-text">Audi R8</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                              View
-                            </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_r8.jpg">                   <div class="card-body">
-                    <p class="card-text">Audi R8</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                              View
-                            </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="/images/audi_r8.jpg">                  <div class="card-body">
-                    <p class="card-text">Audi R8</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                          <a href="./vente/audi_a3.html"  ><button type="button" class="btn btn-sm btn-outline-secondary">
-                              View
-                            </button></a>
-                      </div>
-                      <small class="text-muted">Née sur la piste, faite pour la route.</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  <div class="album py-5 bg-light">
+    <div class="container-shop">      
+      <div class="row">
+        <div class="col-md-4">
+         <form action="" method="get">
+          <button name = "button" type="submit" value="previous">previous</button>
+          <br>
+          <button name = "button" type="submit" value="next">next</button>
+         </form> 
         </div>
+        <div class="col-md-4">
+          <?php
+          
+          require('./productDisplay.php');
+          require('./db_related/pdo_loc.php');
+          try{
+              require("./db_related/config.php");
+          }catch(Exception $e) {
+              throw new Exception("No config ! Incorrect file path or the file is corrupted");
+          }
+          $bdd = db_local::getInstance();
+          $minid = 0;
+          switch ($_GET['button'])
+          {
+              case "previous" :
+              $minid = $minid-4;
+              if($minid < 0){ $minid = 0;}
+              break;
+              case "next" :
+              $minid = $minid+4;
+              break;
+              default :
+              $minid = 0;
+              break;
+          }
+          $requete = $bdd->prepare("SELECT * from listeproduits WHERE IDProduit >= $minid LIMIT 4");
+          $requete->execute();
+          $listproducts = $requete->fetchAll();
+          $products = new Product($listproducts);
+          $products->display($listproducts);
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
 </html>
 
 <?php  
