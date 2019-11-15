b1d543e362fae54ec7346e7e3eb5706541b64ecf
KindPixel <thomas.lima@viacesi.fr>
2019-11-15 10:06:45 +0100

    liste des participants

---
+++ src/listDisplay.php
@@ -0,0 +1,27 @@
+<?php
+class Participant
+{
+	private $listPart;
+
+	function __construct($listPart)
+	{
+		$this->listPart=$listPart;
+	}
+	
+	public function display($listPart){
+    foreach($this->listPart as $rows => $key){
+		echo '
+        <tr>
+            <td>',$key['Prenom'],'</td>
+            <td>',$key['Nom'],'</td>
+        </tr>';
+		}
+	}
+}
+?>
+
+<!-- <tr>
+    <td>Tiger Nixon</td>
+    <td>System Architect</td>
+    <td>Edinburgh</td>
+</tr> -->
