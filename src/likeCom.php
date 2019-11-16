56c5eda9d47a3dd179c74a16aa9221278d83211a
KindPixel <thomas.lima@viacesi.fr>
2019-11-16 17:46:03 +0100

    likecom

---
+++ src/likeCom.php
@@ -0,0 +1,23 @@
+<?php      
+if (!isset($_SESSION)){
+    session_start();
+}
+
+        try{
+            require("./db_related/config.php");
+        }catch(Exception $e) {
+            throw new Exception("No config ! Incorrect file path or the file is corrupted");
+        }
+
+        var_dump($_GET);
+        $id = $_GET['id'];
+
+        $iduser = $_SESSION['IDutilisateur'];
+
+        var_dump($_SESSION['IDutilisateur']);
+
+        $bdd = db_local::getInstance();
+        $requete = $bdd->prepare("INSERT INTO `likescommentaires`(`IDUtilisateur`, `IDCommentaire`) VALUES (1,$id)");
+        $requete->execute();
+        $requete->closeCursor();
+?>
