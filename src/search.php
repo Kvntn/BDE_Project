6951e2a45d25ea78fc74b86213764433052b6a0f
GrineFather <44920911+GrineFather@users.noreply.github.com>
2019-11-17 17:30:27 +0100

    Update search.php

--- src/search.php
+++ src/search.php
@@ -2,7 +2,7 @@
 <div class="autocomplete">
     <form class="form-inline my-2 my-md-0" autocomplete="off" method="post" action="./shop">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">GO</button>
-      <input class="form-control mr-sm-2" type="post" placeholder="Recherhe..." aria-label="Search" id="myInput" type="text" name="search">
+      <input class="form-control mr-sm-2" type="post" placeholder="Recherche..." aria-label="Search" id="myInput" type="text" name="search">
     </form>
 </div>
 
